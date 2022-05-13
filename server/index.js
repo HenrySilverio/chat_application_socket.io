c7519e9b174dd72d498136308dbe7d9db4d605a9
Henry Silverio <silveriohenriqueb@gmail.com>
2022-05-13 11:59:06 -0300

    correcao

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
+const http = require('http')
 const express = require('express')
 const app = express()
-const http = require('http')
 const cors = require('cors')
-const { Server, Socket } = require('socket.io')
+const {Server}  = require('socket.io')
 const router = require('./router/ServerRouter')
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
 
@@ -19,24 +19,26 @@ const io = new Server(server, {
 })
 
 io.on('connection', (socket) => {
-    socket.on('join', ({ name, room }) => {
+    socket.on('join', ({ name, room }, callback) => {
         const { user, error } = addUser({ id: socket.id, name, room })
-
-        if(error) return(error)
-        console.log(user)
+        console.log(socket.id)
+        if(error) return callback(error)
+        
+        socket.join(user.room)
 
         socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}` })
         socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` })
 
         io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) })
-
-        socket.join(user.room)
+        callback()
     })
+    
 
-    socket.on('sendMessage', (message) => {
+    socket.on('sendMessage', (message, callback) => {
         const user = getUser(socket.id)
-
+        console.log(socket.id)
         io.to(user.room).emit('message', { user: user.name, text: message})
+        callback()
     })
 
     socket.on('disconnect', () => {
--- server/users.js
+++ server/users.js
@@ -5,11 +5,12 @@ const addUser = ({ id, name, room }) => {
     room = room.trim().toLowerCase()
 
     const existingUser = users.find((user) => user.room === room && user.name === name)
-
+    console.log(existingUser)
+    if(!name || !room) return { error: 'Username and room are required.' }
     if (existingUser) {
         return { error: 'Username is taken!!!' }
     }
-
+    
     const user = { id, name, room }
 
     users.push(user)
