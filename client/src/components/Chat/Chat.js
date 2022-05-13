a26f5b74294037e74c1de21b4105ba5c7d316571
Gustavo Freitas <freitasgustavos@hotmail.com>
2022-05-13 12:17:17 -0300

    ajustes

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,74 +1,82 @@
-import { useState, useEffect, useMemo, useCallback } from "react";
-import io from 'socket.io-client';
+import { useState, useEffect, useMemo, memo } from "react";
+import io from "socket.io-client";
 import { useLocation } from "react-router-dom";
 
-import TextContainer from '../TextContainer/TextContainer';
-import Messages from '../Messages/Messages';
-import InfoBar from '../InfoBar/InfoBar';
-import Input from '../Input/Input';
+import TextContainer from "../TextContainer/TextContainer";
+import Messages from "../Messages/Messages";
+import InfoBar from "../InfoBar/InfoBar";
+import Input from "../Input/Input";
 
-
-import './Chat.css'
+import "./Chat.css";
 let socket;
 
-function useQuery() {
-    const { search } = useLocation()
-
-    return useMemo(() => new URLSearchParams(search), [search])
-}
-
 function Chat() {
-    let query = useQuery();
-    const [name, setName] = useState(query.get("name"));
-    const [room, setRoom] = useState(query.get("room"));
-    const [users, setUsers] = useState('');
-    const [message, setMessage] = useState('');
-    const [messages, setMessages] = useState([]);
+  let query = useQuery();
 
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [users, setUsers] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
 
-    useEffect(() => {
-        /* const name = query.get("name")
-        const room = query.get("room") */
+  useEffect(() => {
+    const user = query.get("name");
+    const sala = query.get("room");
 
+    setName(user);
+    setRoom(sala);
 
-        socket = io('http://localhost:3002/')
+    socket = io("http://localhost:3002/");
+  }, [query]);
 
-        socket.emit('join', { name, room }, (error) => {
-            if (error) {
-                alert(error);
-            }
-        });
-    }, [])
-
-    useEffect(() => {
-        socket.on('message', message => {
-            setMessages(messages => [...messages, message]);
-        });
-
-        socket.on("roomData", ({ users }) => {
-            setUsers(users);
-        });
-    }, []);
-
-    const sendMessage = (event) => {
-        event.preventDefault();
-
-        if (message) {
-            socket.emit('sendMessage', message, () => setMessage(''));
+  useEffect(() => {
+    if (name) {
+      socket.emit("join", { name, room }, (error) => {
+        if (error) {
+          alert(error);
         }
+      });
     }
+  }, [name, room]);
 
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages((messages) => [...messages, message]);
+    });
 
-    return (
-        <div className="outerContainer">
-            <div className="container">
-                <InfoBar room={room} />
-                <Messages messages={messages} name={name} />
-                <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
-            </div>
-            <TextContainer users={users} />
-        </div>
-    );
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+  }, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+      </div>
+      <TextContainer users={users} />
+    </div>
+  );
 }
 
-export default Chat;
+export default memo(Chat);
+
+function useQuery() {
+  const { search } = useLocation();
+
+  return useMemo(() => new URLSearchParams(search), [search]);
+}
